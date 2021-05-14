d7735a51ecf0443cbabf66894994641e5aca1f0a
Brian Bingham <briansbingham@gmail.com>
2021-05-14 09:13:42 -0700

    tweaks from cloudsim

--- nodes/pointstamped2modelstate.py
+++ nodes/pointstamped2modelstate.py
@@ -11,14 +11,16 @@ import rospy
 
 from geometry_msgs.msg import PointStamped
 from gazebo_msgs.msg import ModelState
+from gazebo_msgs.srv import SetModelState
 
 class Node():
     def __init__(self, model_name, reference_frame):
         self.modelmsg = ModelState()
         self.modelmsg.model_name = model_name
         self.modelmsg.reference_frame = reference_frame
-        self.outpub = rospy.Publisher("/gazebo/set_model_state",ModelState,queue_size=1)
-
+        self.outpub = rospy.Publisher("/gazebo/set_model_state",ModelState,queue_size=10)
+        #rospy.wait_for_service('/gazebo/set_model_state')
+        #self.setservice = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
 
     def point_callback(self,msg):
         self.modelmsg.pose.position.x = msg.point.x
@@ -26,6 +28,7 @@ class Node():
         self.modelmsg.pose.position.z = msg.point.z
 
         self.outpub.publish(self.modelmsg)
+        #resp = self.setservice(self.modelmsg)
 
         
 if __name__ == '__main__':
